a0313f0cbec3f4fe7961440430816d71d7d64808
Lucas Chaves <chaveslucas14@gmail.com>
2024-05-31 15:12:05 -0300

    ambient

--- src/utils/getAmbientURL.ts
+++ src/utils/getAmbientURL.ts
@@ -1,11 +1,11 @@
 export const getAmbientURL = () => {
-    if (
-        ["localhost", import.meta.env.VITE_IP].includes(
-            window.location.hostname
-        )
-    ) {
-        return import.meta.env.VITE_BASE_URL;
-    }
+    // if (
+    //     ["localhost", import.meta.env.VITE_IP].includes(
+    //         window.location.hostname
+    //     )
+    // ) {
+    return import.meta.env.VITE_BASE_URL;
+    // }
 
-    return `${window.origin}/api/v1`;
+    // return `${window.origin}/api/v1`;
 };
